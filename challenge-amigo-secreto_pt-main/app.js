e8295403be9c923ac9ebbd550fa45c623495f591
Diego Santos <didadss100@gmail.com>
2025-02-12 17:08:02 -0300

    Implementando função para agregar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,19 @@
+let nomesDosAmigos = [];
 
+function adicionarAmigo() {
+    let nomes = document.querySelector('#amigo').value;
+    
+    if (nomes === '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        nomesDosAmigos.push(nomes);
+        console.log(nomesDosAmigos)// Para verificar se o nome foi adicionado
+        limparCampo();
+    }
+}
+function limparCampo () {
+   let nomes = document.querySelector('#amigo');
+   nomes.value = '';
+}
+
+    
